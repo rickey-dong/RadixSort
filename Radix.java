cad666ef65b173932327eac719a1f567f11fe04c
Rickey Dong <rickeyd2004@gmail.com>
2021-01-12 21:54:58 -0500

    modified merge method to take SortableLinkedList instead of MyLinkedList

--- Radix.java
+++ Radix.java
@@ -16,7 +16,7 @@ public class Radix
     int len = ((int) Math.log10(n)) + 1;
     return len;
   }
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets)
+  public static void merge(SortableLinkedList original, SortableLinkedList[] buckets)
   {
     for (int i = 0; i < buckets.length; i++)
     {
