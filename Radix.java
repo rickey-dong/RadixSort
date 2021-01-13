40231bc38400576b9e8628ef7f8715def3e0cd4d
Rickey Dong <rickeyd2004@gmail.com>
2021-01-12 21:36:15 -0500

    created the length method to find how many digits in a number

--- Radix.java
+++ Radix.java
@@ -6,4 +6,21 @@ public class Radix
     int extractedDigit = (int) (n % Math.pow(10, col+1)) / (int) Math.pow(10, col);
     return extractedDigit;
   }
+  public static int length(int n)
+  {
+    if (n == 0)
+    {
+      return 1;
+    }
+    n = Math.abs(n);
+    int len = ((int) Math.log10(n)) + 1;
+    return len;
+  }
+  public static void merge(MyLinkedList original, MyLinkedList[] buckets)
+  {
+    for (int i = 0; i < buckets.length; i++)
+    {
+      original.extend(buckets[i]);
+    }
+  }
 }
--- RadixTesterRD.java
+++ RadixTesterRD.java
@@ -12,5 +12,22 @@ public class RadixTesterRD
     System.out.println(Radix.nth(-12345, 2) + " should be 3");
     System.out.println(Radix.nth(-12345, 3) + " should be 2");
     System.out.println(Radix.nth(-12345, 4) + " should be 1");
+    
+    System.out.println();
+    
+    System.out.println(Radix.length(0) + " should be 1");
+    System.out.println(Radix.length(1) + " should be 1");
+    System.out.println(Radix.length(11) + " should be 2");
+    System.out.println(Radix.length(111) + " should be 3");
+    System.out.println(Radix.length(1111) + " should be 4");
+    System.out.println(Radix.length(11111) + " should be 5");
+    System.out.println(Radix.length(9) + " should be 1");
+    System.out.println(Radix.length(-0) + " should be 1");
+    System.out.println(Radix.length(-1) + " should be 1");
+    System.out.println(Radix.length(-11) + " should be 2");
+    System.out.println(Radix.length(-111) + " should be 3");
+    System.out.println(Radix.length(-1111) + " should be 4");
+    System.out.println(Radix.length(-11111) + " should be 5");
+    System.out.println(Radix.length(-9) + " should be 1");
   }
 }
